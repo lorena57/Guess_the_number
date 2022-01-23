55d5d5e7320ff1fddb8f61168306dcc05e73f2af
Lorena C <lorena@eunknown.org>
2022-01-22 19:08:52 -0800

    create query selectors to manipulate dom

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let newMessage = (document.querySelector('.message').textContent =
+  'Correct number');
+
+let guessedNumber = (document.querySelector('.number').textContent = 50);
+let playerScore = (document.querySelector('.score').textContent = 9);
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
