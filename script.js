f3dcff3b9c83ee7a895870e3cc9927d0ea16af54
Lorena C <lorena@eunknown.org>
2022-01-22 19:26:23 -0800

    add event listner

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 'use strict';
 
-let newMessage = (document.querySelector('.message').textContent =
-  'Correct number');
+// let newMessage = (document.querySelector('.message').textContent =
+//   'Correct number');
 
-let guessedNumber = (document.querySelector('.number').textContent = 50);
-let playerScore = (document.querySelector('.score').textContent = 9);
+// let guessedNumber = (document.querySelector('.number').textContent = 50);
+// let playerScore = (document.querySelector('.score').textContent = 9);
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number selected';
+  }
+});
