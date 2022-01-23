634c47e14ccb70cd5da68a5b496e3cbc5fd65dc5
Lorena C <lorena@eunknown.org>
2022-01-23 12:27:49 -0800

    create variable so hold score

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let guessedNumber = (document.querySelector('.number').textContent =
   secretNumber);
 
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
-  console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number selected';
@@ -16,7 +16,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Correct number';
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = 'Number too high';
+    score--;
+    document.querySelector('.score').textContent = score;
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'Number too low';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
