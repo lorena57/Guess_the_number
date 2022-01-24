f79f654fe5e787e97bfdc9c713f4644f35e5bcc0
Lorena C <lorena@eunknown.org>
2022-01-23 18:20:51 -0800

    add background color

--- script.js
+++ script.js
@@ -10,10 +10,17 @@ let score = 20;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  //When no number is selected
   if (!guess) {
     document.querySelector('.message').textContent = 'No number selected';
+
+    //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Number too high';
@@ -23,6 +30,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost';
       document.querySelector('.score').textContent = 0;
     }
+
+    //When number is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Number too low';
